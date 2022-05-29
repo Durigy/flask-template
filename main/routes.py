dae508c5b3192a8c7c3d150f16cee95789c646f5
Alexander Deverson <56241583+Durigy@users.noreply.github.com>
2022-05-29 09:45:57 +0100

    updated generate_id func

--- main/routes.py
+++ main/routes.py
@@ -48,19 +48,20 @@ def index():
 def home_redirect():
     return redirect(url_for('index'))
 
-# this can be used for generating ids of string lenght 20 
-# or the default in modules if fine for small projects
-def generate_id(query_table):
+# this can be used for generating ids of string lenght 20 default
+# or the default in modules can be used for small projects if preferred
+def generate_id(query_table, id_length = 20):
     id = ''
 
     while True:
         if not check_id(query_table, id): 
             break
-        id = secrets.token_hex(10)
+        id = secrets.token_hex(round(abs(id_length/2)))
+    id = id[:id_length]
     
     return id
 
-# returns turn is no an id is found in the database
+# returns true if no an id is found in the database
 def check_id(query_table, query_id):
     ans = query_table.query.filter(query_table.id == query_id).first()
     return True if ans else False
